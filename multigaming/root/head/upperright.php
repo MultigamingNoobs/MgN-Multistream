92d71926d1b4b1867b363e499287eb49cc536526
MarderLP <Marder001@gmail.com>
2014-04-28 18:32:28 +0200

    v.0.2.11B
    kleiner Fehler bei PNGs refresh behoben

--- multigaming/root/head/upperright.php
+++ multigaming/root/head/upperright.php
@@ -1,5 +1,5 @@
 <?php
-	echo '<a href="multigaming/root/head/pngs.php'.makeList(getAllStreams()).'" target="pngs">PNGs refreshen</a>';
+	echo '<a href="multigaming/root/head/pngs.php?streams='.makeList(getAllStreams()).'" target="pngs">PNGs refreshen</a>';
 	echo '<br>';
 	echo '<a href='.getStreamString().' target="content">Streams refreshen</a>';
 ?>
