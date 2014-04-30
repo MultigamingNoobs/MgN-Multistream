f65863bee19794588db4e894bf3c81eac3ea46bc
MarderLP <Marder001@gmail.com>
2014-04-30 20:26:47 +0200

    v.0.2.12
    fixed höhenfehler bei debug=hitboxchat
    added tooltips bei menülinks

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<link href="multigaming/css/design.css" type="text/css" rel="stylesheet">
 		<?php
-			$v = "BETA v.0.2.11B";
+			$v = "BETA v.0.2.12";
 			include 'multigaming/api/api.php';
 		?>
 		<title>MultiGaming</title>
--- multigaming/root/head/upperright.php
+++ multigaming/root/head/upperright.php
@@ -1,5 +1,5 @@
 <?php
-	echo '<a href="multigaming/root/head/pngs.php?streams='.makeList(getAllStreams()).'" target="pngs">PNGs refreshen</a>';
+	echo '<a href="multigaming/root/head/pngs.php?streams='.makeList(getAllStreams()).'" target="pngs" title="Lädt die PNGs neu, nur die PNGs">PNGs refreshen</a>';
 	echo '<br>';
-	echo '<a href='.getStreamString().' target="content">Streams refreshen</a>';
+	echo '<a href='.getStreamString().' target="content" title="Selber Effekt wie bei Startseite">Streams refreshen</a>';
 ?>
