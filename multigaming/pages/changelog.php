24873fd517f633a3f8c7a7db7781405852f02758
MarderLP <Marder001@gmail.com>
2014-05-02 13:12:13 +0200

    v.0.2.13_3
    nothing important
    some <i> and <b>-tags in changelog

--- multigaming/pages/changelog.php
+++ multigaming/pages/changelog.php
@@ -1,73 +1,75 @@
 <link href="../css/changes.css" type="text/css" rel="stylesheet">
 <ul id="changes">
-	<li>Probleme? Teilt sie mir mit!
+	<li><b>Probleme? Teilt sie mir mit!</b>
 		<ul>
 		<li>Schreib mir eine EMail mit deinen Problemen oder Ideen!</li>
 		<li>Benutze dazu einfach den Link im Menü.</li>
 		</ul>
 	</li>
 	<br>
-	<li>Geplante Funktionen
+	<li><b>Geplante Funktionen</b>
 		<ul>
 		<li>Hitbox-Chat anzeigen (auf eine schönere Art)</li>
 		</ul>
 	</li>
 	<br>
 	<hr>
-	<li>BETA v.0.2.13
+	<li><b>BETA v.0.2.13</b>
 		<ul>
-		<li><p>fixxed: Seite ist nun nicht mehr größer als der Sichtbare Fenster bereich. D.h. ab einer Gewissen Anzahl an Streams, können diese separat gescrollt werden.</p></li>
-		<li><p>fixxed: Streamgröße bei 8+ gleichtig</p></li>
-		<li><p>fixxed: Anzeige bei keiner online</p></li>
-		</ul>
-	</li>
-	<hr>
-	<li>BETA v.0.2.12
-		<ul>
-		<li>added: Tooltips bei Menülinks</li>
-		<li>fixed: Streamhöhe bei hitboxchat</li>
+		<li><i>fixxed:</i> Seite ist nun nicht mehr größer als der Sichtbare Fenster bereich. D.h. ab einer Gewissen Anzahl an Streams, können diese separat gescrollt werden.</li>
+		<li><i>fixxed:</i> Streamgröße bei 8+ gleichtig</li>
+		<li><i>fixxed:</i> Anzeige bei keiner online</li>
 		</ul>
 	</li>
 	<br>
-	<li>BETA v.0.2.11B
+	<li><b>BETA v.0.2.12</b>
 		<ul>
-		<li>fixxed: PNG refresh Link-Fehler behoben.<li>
+		<li><i>added:</i> Tooltips bei Menülinks</li>
+		<li><i>fixed:</i> Streamhöhe bei hitboxchat</li>
 		</ul>
 	</li>
-	<br>
-	<li>BETA v.0.2.11
+	<li><b>BETA v.0.2.11B</b>
 		<ul>
-		<li>changed: Struktur der Daten wurde verändert</li>
-		<li>changed: Header als IFrame, separates neuladen möglich</li>
-		<li>changed: Content als IFrame, schnelleres Laden der Seiten, da PNG laden entfällt</li>
-		<li>changed: ?beta wird zu ?debug, warum auch immer es ?beta war</li>
-		<li>added: debug=hitboxchat wurde hinzugefügt. Die Anzeige wird später anders gestaltet</li>
+		<li><i>fixxed:</i> PNG refresh Link-Fehler behoben.<li>
 		</ul>
 	</li>
-	<li>BETA v.0.2.10
+	<li><b>BETA v.0.2.11</b>
 		<ul>
-		<li>added: Workaround - bei mehr als 2 Streams wird die höhe nicht auf maximal 100% der Seite angepasst</li>
-		<li>changed: bis zu 8 Streams ohne Scrollen, vorher waren es bis zu 10</li>
-		<li>added: Link zum EMail schreiben der im Footer war nun in das Kopfmenü übernommen</li>
-		<li>added: Geplante Funktionen</li>
+		<li><i>changed:</i> Struktur der Daten wurde verändert</li>
+		<li><i>changed:</i> Header als IFrame, separates neuladen möglich</li>
+		<li><i>changed:</i> Content als IFrame, schnelleres Laden der Seiten, da PNG laden entfällt</li>
+		<li><i>changed:</i> ?beta wird zu ?debug, warum auch immer es ?beta war</li>
+		<li><i>added:</i> debug=hitboxchat wurde hinzugefügt. Die Anzeige wird später anders gestaltet</li>
 		</ul>
 	</li>
-	<li>BETA v.0.2.9
+	<li><b>BETA v.0.2.10</b>
 		<ul>
-		<li>changed: kein Fußbereich mehr, dafür ein Menü oberhalb</li>
-		<li>changed: ein Stream wir groß angezeit, bei mehr werden 2 nebeneinander angezeigt</li>
-		<li>changed: es sollten bis zu 10 Streams ohne scollen angezeigt werden können (in den besagten 2er-Reihen)</li>
-		<li>added: Hilfe und Changelog</li>
+		<li><i>added:</i> Workaround - bei mehr als 2 Streams wird die höhe nicht auf maximal 100% der Seite angepasst</li>
+		<li><i>changed:</i> bis zu 8 Streams ohne Scrollen, vorher waren es bis zu 10</li>
+		<li><i>added:</i> Link zum EMail schreiben der im Footer war nun in das Kopfmenü übernommen</li>
+		<li><i>added:</i> Geplante Funktionen</li>
 		</ul>
 	</li>
-	<li>BETA v.0.2.8-
+	<li><b>BETA v.0.2.9</b>
 		<ul>
-		<li>changed: Keine html-Tabellen mehr!</li>
-		<li>fixed: Die Räume die im IRC eingetragen werden sind nun auch vorhanden</li>
-		<li>added: Test Modus, Streams anzeigen, auch wenn diese offline sind (?beta=offline)</li>
+		<li><i>changed:</i> kein Fußbereich mehr, dafür ein Menü oberhalb</li>
+		<li><i>changed:</i> ein Stream wir groß angezeit, bei mehr werden 2 nebeneinander angezeigt</li>
+		<li><i>changed:</i> es sollten bis zu 10 Streams ohne scollen angezeigt werden können (in den besagten 2er-Reihen)</li>
+		<li><i>added:</i> Hilfe und Changelog</li>
 		</ul>
 	</li>
-	<li>BETA v.0.1.0
+	<li><b>BETA v.0.2.8</b>
+		<ul>
+		<li><i>fixed:</i> Die Räume die im IRC eingetragen werden sind nun auch vorhanden</li>
+		<li><i>added:</i> Test Modus, Streams anzeigen, auch wenn diese offline sind (?beta=offline)</li>
+		</ul>
+	</li>
+	<li><b>BETA v.0.2.0</b>
+		<ul>
+		<li><i>changed:</i> Keine html-Tabellen mehr!</li>
+		</ul>
+	</li>
+	<li><b>BETA v.0.1.0</b>
 		<ul>
 		<li>Erste simple Testversion mit html-Tabelle</li>
 		</ul>
