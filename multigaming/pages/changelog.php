203253708f9a6115fb34bc6057ba63b6c20bb8ee
MarderLP <Marder001@gmail.com>
2014-05-28 08:33:23 +0200

    umlautfix
    umlaute geändert in impressum, help und changelog

--- multigaming/pages/changelog.php
+++ multigaming/pages/changelog.php
@@ -3,13 +3,13 @@
 	<li><b>Probleme? Teilt sie mir mit!</b>
 		<ul>
 		<li>Schreib mir eine EMail mit deinen Problemen oder Ideen!</li>
-		<li>Benutze dazu einfach den Link im Men�.</li>
+		<li>Benutze dazu einfach den Link im Men&uuml;.</li>
 		</ul>
 	</li>
 	<br>
 	<li><b>Geplante Funktionen</b>
 		<ul>
-		<li>Hitbox-Chat anzeigen (auf eine sch�nere Art)</li>
+		<li>Hitbox-Chat anzeigen (auf eine sch&ouml;nere Art)</li>
 		</ul>
 	</li>
 	<br>
@@ -19,7 +19,7 @@
 		<ul>
 		<li><i>changed:</i> Startseite umbenannt zu Streams</li>
 		<li><i>changed:</i> Neues Impressum</li>
-		<li><i>changed:</i> </li>
+		<li><i>fixxed:</i> Umlaut-Fix</li>
 		</ul>
 	</li>
 	<li><b>BETA v.0.2.15</b>
@@ -29,26 +29,26 @@
 	</li>
 	<li><b>BETA v.0.2.14</b>
 		<ul>
-		<li><i>fixxed:</i> Kleine �nderungen, sodass online Streams aktualisiert werden, wenn man die Streamseite neu l�dt.</li>
+		<li><i>fixxed:</i> Kleine &Auml;nderungen, sodass online Streams aktualisiert werden, wenn man die Streamseite neu l&auml;dt.</li>
 		</ul>
 	</li>
 	<li><b>BETA v.0.2.13B</b>
 		<ul>
-		<li><i>changed:</i> PNG refresh ist nun im Men�</li>
+		<li><i>changed:</i> PNG refresh ist nun im Men&uuml;</li>
 		<li><i>fixxed:</i> ein paar kleine Dinge im Hintergrund</li>
 		</ul>
 	</li>
 	<li><b>BETA v.0.2.13</b>
 		<ul>
-		<li><i>fixxed:</i> Seite ist nun nicht mehr gr��er als der Sichtbare Fenster bereich. D.h. ab einer Gewissen Anzahl an Streams, k�nnen diese separat gescrollt werden.</li>
-		<li><i>fixxed:</i> Streamgr��e bei 8+ gleichtig</li>
+		<li><i>fixxed:</i> Seite ist nun nicht mehr gr&ouml;&szlig;er als der Sichtbare Fenster bereich. D.h. ab einer Gewissen Anzahl an Streams, k&ouml;nnen diese separat gescrollt werden.</li>
+		<li><i>fixxed:</i> Streamgr&ouml;&szlig;e bei 8+ gleichtig</li>
 		<li><i>fixxed:</i> Anzeige bei keiner online</li>
 		</ul>
 	</li>
 	<li><b>BETA v.0.2.12</b>
 		<ul>
-		<li><i>added:</i> Tooltips bei Men�links</li>
-		<li><i>fixed:</i> Streamh�he bei hitboxchat</li>
+		<li><i>added:</i> Tooltips bei Men&uuml;links</li>
+		<li><i>fixed:</i> Streamh&ouml;he bei hitboxchat</li>
 		</ul>
 	</li>
 	<li><b>BETA v.0.2.11B</b>
@@ -58,32 +58,32 @@
 	</li>
 	<li><b>BETA v.0.2.11</b>
 		<ul>
-		<li><i>changed:</i> Struktur der Daten wurde ver�ndert</li>
-		<li><i>changed:</i> Header als IFrame, separates neuladen m�glich</li>
-		<li><i>changed:</i> Content als IFrame, schnelleres Laden der Seiten, da PNG laden entf�llt</li>
+		<li><i>changed:</i> Struktur der Daten wurde ver&auml;ndert</li>
+		<li><i>changed:</i> Header als IFrame, separates neuladen m&ouml;glich</li>
+		<li><i>changed:</i> Content als IFrame, schnelleres Laden der Seiten, da PNG laden entf&uuml;llt</li>
 		<li><i>changed:</i> ?beta wird zu ?debug, warum auch immer es ?beta war</li>
-		<li><i>added:</i> debug=hitboxchat wurde hinzugef�gt. Die Anzeige wird sp�ter anders gestaltet</li>
+		<li><i>added:</i> debug=hitboxchat wurde hinzugef&uuml;gt. Die Anzeige wird sp&auml;ter anders gestaltet</li>
 		</ul>
 	</li>
 	<li><b>BETA v.0.2.10</b>
 		<ul>
-		<li><i>added:</i> Workaround - bei mehr als 2 Streams wird die h�he nicht auf maximal 100% der Seite angepasst</li>
+		<li><i>added:</i> Workaround - bei mehr als 2 Streams wird die h&ouml;he nicht auf maximal 100% der Seite angepasst</li>
 		<li><i>changed:</i> bis zu 8 Streams ohne Scrollen, vorher waren es bis zu 10</li>
-		<li><i>added:</i> Link zum EMail schreiben der im Footer war nun in das Kopfmen� �bernommen</li>
+		<li><i>added:</i> Link zum EMail schreiben der im Footer war nun in das Kopfmen&uuml; &uuml;bernommen</li>
 		<li><i>added:</i> Geplante Funktionen</li>
 		</ul>
 	</li>
 	<li><b>BETA v.0.2.9</b>
 		<ul>
-		<li><i>changed:</i> kein Fu�bereich mehr, daf�r ein Men� oberhalb</li>
-		<li><i>changed:</i> ein Stream wir gro� angezeit, bei mehr werden 2 nebeneinander angezeigt</li>
-		<li><i>changed:</i> es sollten bis zu 10 Streams ohne scollen angezeigt werden k�nnen (in den besagten 2er-Reihen)</li>
+		<li><i>changed:</i> kein Fu&szlig;bereich mehr, daf&uuml;r ein Men&uuml; oberhalb</li>
+		<li><i>changed:</i> ein Stream wir gro&szlig; angezeit, bei mehr werden 2 nebeneinander angezeigt</li>
+		<li><i>changed:</i> es sollten bis zu 10 Streams ohne scollen angezeigt werden k&ouml;nnen (in den besagten 2er-Reihen)</li>
 		<li><i>added:</i> Hilfe und Changelog</li>
 		</ul>
 	</li>
 	<li><b>BETA v.0.2.8</b>
 		<ul>
-		<li><i>fixed:</i> Die R�ume die im IRC eingetragen werden sind nun auch vorhanden</li>
+		<li><i>fixed:</i> Die R&auml;ume die im IRC eingetragen werden sind nun auch vorhanden</li>
 		<li><i>added:</i> Test Modus, Streams anzeigen, auch wenn diese offline sind (?beta=offline)</li>
 		</ul>
 	</li>
