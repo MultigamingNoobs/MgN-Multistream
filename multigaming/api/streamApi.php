453a6bc91f32e14e2c1241bfcdb6242cfed218fb
MarderLP <Marder001@gmail.com>
2014-06-10 14:21:37 +0200

    0.4.1 last
    added pace
    added twitter widget
    some code cleaning

--- multigaming/api/streamApi.php
+++ multigaming/api/streamApi.php
@@ -10,34 +10,22 @@
 		return $t;
 	}
 
-	function getStreamString(){
-		$debug = '';
-		if(strlen($_REQUEST['debug']) > 0){
-			$debug = '&debug=' . $_GET['debug'];
-		}
-		return '"multigaming/pages/streams.php?hitbox='.getHitboxStreamString().'&twitch='.getTwitchStreamString().$debug.'"';
-	}
-		
-	//chat
 	function displaySidebarChat($twitch,$hitbox){
-		if(count($twitch)+count($hitbox) == 0){
+		for($i=0;$i<count($twitch);$i++){
 			echo '<div class="tabbertab">';
-			echo '<h2>keiner Online</h2>';
-			echo '<p>Sobald ein Stream online ist, wird der Chat dazu angezeigt.</p>';
+			echo '<h2>'.$twitch[$i].'</h2>';
+				displayTwitchChat($twitch[$i],'100',false);
 			echo '</div>';
-		}else{
-			for($i=0;$i<count($twitch);$i++){
-				echo '<div class="tabbertab">';
-				echo '<h2>'.$twitch[$i].'</h2>';
-					displayTwitchChat($twitch[$i],'100',false);
-				echo '</div>';
-			}
-			for($i=0;$i<count($hitbox);$i++){
-				echo '<div class="tabbertab">';
-				echo '<h2>'.$hitbox[$i].'</h2>';
-					displayHitboxChat($hitbox[$i],'100',false);
-				echo '</div>';
-			}
 		}
+		for($i=0;$i<count($hitbox);$i++){
+			echo '<div class="tabbertab">';
+			echo '<h2>'.$hitbox[$i].'</h2>';
+				displayHitboxChat($hitbox[$i],'100',false);
+			echo '</div>';
+		}
+		echo '<div class="tabbertab">';
+		echo '<h2>Twitter</h2>';
+			include 'multigaming/pages/twitter.php';
+		echo '</div>';
 	}
 ?>
