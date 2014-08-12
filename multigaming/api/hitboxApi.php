c58c8c037258f255fefbae26959b45fd181909d5
MarderLP <Marder001@gmail.com>
2014-08-12 12:00:34 +0200

    Revert "Revert "v.0.7.1 chat margin""
    This reverts commit a9eb81250501493e79093d86ace1664a4f1021b0.

--- multigaming/api/hitboxApi.php
+++ multigaming/api/hitboxApi.php
@@ -121,6 +121,6 @@
 		}
 	}
 	function displayHitboxChat($stream,$h){
-		echo '<iframe class="ChatBox" id="Chat0" rel="0" width=100% height=100% src ="http://www.hitbox.tv/embedchat/' . $stream . '" frameborder="0" style="display: inline;margin-top: 21px;"></iframe>';				
+		echo '<iframe class="ChatBox" id="Chat0" rel="0" width=100% height=100% src ="http://www.hitbox.tv/embedchat/' . $stream . '" frameborder="0" style="display: inline;margin-top: 4px;"></iframe>';				
 	}
 ?>
