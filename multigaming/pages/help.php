1f9edb4e0cd5f2b68eb1d3f916f69ce938c408a6
MarderLP <Marder001@gmail.com>
2014-08-13 12:26:16 +0200

    new help
    removed some unimportand stuff from help and added a how to bed in mgn
    multistream

--- multigaming/pages/help.php
+++ multigaming/pages/help.php
@@ -1,49 +1,10 @@
 <div id="content">
 <ul class="sites">
-	<li><b>Die Hilfe - Inhalt</b>
-		<ul>
-			<li>Die Idee</li>
-			<li>Das Team</li>
-			<li>Der Chat</li>
-			<li>Streams</li>
-			<li>Adresszeilenkomandos</li>
-		</ul>
-	</li>
-	<li></li>
-</ul>
-<br>
-<ul class="sites">
-	<li></li>
-	<li><b>Die Idee</b>
-		<ul>
-			<li>Wir sind eine kleine Gruppe von Streamern, die miteinander zocken oder auch bei den anderen zuschauen.</li>
-			<li>Um nun euch unsere Gruppe n&auml;her zu bringen gibt es hier jeden Stream zu sehen, ohne gro&szlig; hin und her zu tabben. Wer online ist wird angezeigt, so einfach ist das! (Sofern bei der Startseite nicht das H&auml;kchen bei <i>Keine MGN Streams anzeigen</i> gemacht wurde)</li>
-			<li>Nat&uuml;rlich ist es auch m&ouml;glich weitere Streamer &uuml;ber diese Seite zu gucken, die nicht in unserer Gruppe sind. Dazu einfach auf der Startseite die gew&uuml;nschtn Streamer eintragen und los geht es! Die Eingabe wird &uuml;ber die Adresszeile ausgewertet, siehe dazu den Abschnitt <b>Adresszeilenkomandos</b>.</li>
-		</ul>
-	</li>
-	<li></li>
-	<li><b>Das Team</b>
-		<ul>
-			<li>Die Seite wird derzeit von MarderLP betreut und erstellt.</li>
-		</ul>
-	</li>
-	<li></li>
-	<li><b>Der Chat</b>
-		<ul>
-			<li>Zu jedem Stream der online ist, wird automatisch in der rechten Navigation der dazugeh&ouml;rige Chat in einem Tabbed-Men&uuml; angezeigt.</li>
-		</ul>
-	</li>
-	<li></li>
-	<li><b>Streams</b>
-		<ul>
-			<li>Alle Streams die werden angezeigt, wenn sie online sind. Bei Hitbox wird ein HTML5 Player eingebunden und bei Twitch (auf Grund von Problemen) der Flash-Player.</li>
-		</ul>
-	</li>
 	<li></li>
 	<li><b>Adresszeilenkomandos</b>
 		<ul>
 			<li>Im Folgenden sind alle unterst&uuml;tzten Eingaben gelistet, auf die die Startseite zur&uuml;ck greift. Darauf folgen ein paar Beispiele.></li>
-			<li><u>streams</u>
+			<li><u>Streams</u>
 				<ul>
 					<li>twitch - Liste f&uuml;r twitch Streams</li>
 					<li>hitbox - Liste f&uuml;r hitbox Streams</li>
@@ -61,15 +22,41 @@
 					<li>tab=changelog - Changelog</li>
 				</ul>
 			</li>
+			<li><u>Sprache</u>
+				<ul>
+					<li>Es stehen folgende Sprachen zur Auswahl, wobei Englisch standart ist:</li>
+					<li>lang=German - Deutsch</li>
+					<li>lang=English - Endlisch</li>
+				</ul>
+			</li>
 			<li><u>Team und Empfehlungen</u>
 				<ul>
-					<li>Sobald team=on oder suggestions=on ist, werden die Streams der MgN bzw. Empfehlungen im Streams Tab angezeigt.</li>
+					<li>team - Parameter f&uuml;r MgN Streams</li>
+					<li>suggestions - Parameter f&uuml;r weitere Empfehlungen</li>
+					<li>Sobald team=on oder suggestions=on ist, werden die Streams der MgN bzw. Empfehlungen im Streams Tab angezeigt. Dies hat auf den Tab MgN keinen Einfluss, dort werden immer die MgN Tabs angezeigt.</li>
 				</ul>
 			</li>
 			<li><u>Beispiele</u>
 				<ul>
 					<li>?hitbox=user,user2&amp;twitch=user3,user4&amp;tab=streams - Die Reihenfolge ist egal, wichtig ist, dass am Anfang der Eingabe einmal ein <i>?</i> steht und die verschiedenen Parameter durch ein &amp; verbunden sind.</li>
 				</ul>
+			</li>
+		</ul>
+	</li>
+	<li></li>
+	<li><b>Seite einbetten</b>
+		<ul>
+			<li>Wenn du Multistream auf deiner Seite einbetten willst, kannst du das wie folgt machen:</li>
+			<li>Dabei sind {twitchstreams} und {hitboxstreams} durch Kommata getrennte Listen wie oben bei den Adresszeilenkommandos</li>
+			<li><u>Streams</u>
+				<code>
+					&lt;iframe src=&quot;http://stream.my-mgn.de/multigaming/pages/showStreams.php?twitch={twitchstreams}&amp;hitbox={hitboxstreams}&quot; name=&quot;mgnmultistream&quot; width=&quot;500&quot; height=&quot;400&quot;&gt;&lt;/iframe&gt;
+				</code>
+			</li>
+			<li><u>Chat</u>
+				<code>
+					&lt;iframe src=&quot;http://stream.my-mgn.de/multigaming/pages/showChat.php?twitch={twitchstreams}&amp;hitbox={hitboxstreams}&quot; name=&quot;mgnmultistream&quot; width=&quot;500&quot; height=&quot;400&quot;&gt;&lt;/iframe&gt;
+				</code>
 			</li>
 		</ul>
 	</li>
