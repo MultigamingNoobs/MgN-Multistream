32d29ced84294dd19274869b5049e9d035e1fca7
MarderLP <Marder001@gmail.com>
2014-08-14 15:30:13 +0200

    order of code
    code order like in showChat and showStreams

--- multigaming/pages/popoutChat.php
+++ multigaming/pages/popoutChat.php
@@ -7,25 +7,25 @@
 	</head>
 	<body>
 		<?php
-			include '../api/hitboxApi.php';
-			include '../api/twitchApi.php';
-			include '../api/streamApi.php';
-		
 			$hitbox = split(',',$_GET['hitbox']);
 			$twitch = split(',',$_GET['twitch']);
-		
-			$hitboxOnline = getOnlineHitboxStreams($hitbox);
-			$twitchOnline = getOnlineTwitchStreams($twitch);
-					
+			
 			$lang = 'english';
 			if($_GET['lang'] != null and $_GET['lang'] != ''){
 				$lang = strtolower($_GET['lang']);
 			}
+			
 			include '../api/language/'.$lang.'.php';
-		
+			include '../api/hitboxApi.php';
+			include '../api/twitchApi.php';
+			include '../api/streamApi.php';
+			
+			$hitboxOnline = getOnlineHitboxStreams($hitbox);
+			$twitchOnline = getOnlineTwitchStreams($twitch);
+				
 			echo '<div id="chat_">';
 				echo '<div class="tabber">';
-					displaySidebarChat($twitchOnline,$hitboxOnline);
+					displaySidebarChat($hitboxOnline,$twitchOnline);
 				echo '</div>';
 			echo '</div>';
 		 ?>
