2f563611d4b000d835d094571d869bc657d8b005
MarderLP <Marder001@gmail.com>
2014-11-04 14:49:23 +0100

    youtuber in a for-loop

--- mg/p/youtuber.php
+++ mg/p/youtuber.php
@@ -1,7 +1,7 @@
-<iframe frameborder="0" id="youtuber" scrolling="no" src="http://www.youtube.com/subscribe_widget?p=Marder007006005"></iframe>
-<iframe frameborder="0" id="youtuber" scrolling="no" src="http://www.youtube.com/subscribe_widget?p=teiwazlp"></iframe>
-<iframe frameborder="0" id="youtuber" scrolling="no" src="http://www.youtube.com/subscribe_widget?p=damakash"></iframe>
-<iframe frameborder="0" id="youtuber" scrolling="no" src="http://www.youtube.com/subscribe_widget?p=NephtisGaming"></iframe>
-<iframe frameborder="0" id="youtuber" scrolling="no" src="http://www.youtube.com/subscribe_widget?p=MgNB3rZ3rK3r"></iframe>
-<iframe frameborder="0" id="youtuber" scrolling="no" src="http://www.youtube.com/subscribe_widget?p=tomme90201"></iframe>
-<iframe frameborder="0" id="youtuber" scrolling="no" src="http://www.youtube.com/subscribe_widget?p=MgNKater"></iframe>
+<?php
+	$youtuber = array("marder007006005","teiwazlp","damakash","nephtisgaming","mgNB3rZ3rK3r","tomme90201","mgNKater");
+	sort($youtuber);
+	for($i=0;$i<count($youtuber);$i++){
+		echo '<iframe frameborder="0" id="youtuber" scrolling="no" src="http://www.youtube.com/subscribe_widget?p='.$youtuber[$i].'"></iframe>';
+	}
+?>
