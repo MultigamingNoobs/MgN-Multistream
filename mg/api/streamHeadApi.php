a9247c070822e7c0323f01318aede18af15341db
MarderLP <Marder001@gmail.com>
2014-11-07 00:56:02 +0100

    added qrcode
    added functions to show a qrcode with mouseover
    added css for qrmouseover
    added css for stream-channel-mouseover (comming soone)

--- mg/api/streamHeadApi.php
+++ mg/api/streamHeadApi.php
@@ -4,25 +4,24 @@
 		$c = count($hitboxOnline)+count($twitchOnline);
 		//loop to show the stream informations
 		for($i=1;$i<$c+1;$i++){
-			//check weather a new row has to be inserted to the table
-				echo '<td>';
-					echo '<table id="middleTable">';
-						echo '<tr>';
-							if($i<count($hitboxOnline)+1){
-								//show hitboxOnline stream info
-								displayHitboxStreamInfo($hitboxOnline[$i-1]);
-							}else{
-								//show twitchOnline stream info
-								displayTwitchStreamInfo($twitchOnline[$i-1-count($hitboxOnline)]);
-							}
-						echo '</tr>';
-					echo '</table>';
-				echo '</td>';
+			echo '<td>';
+				echo '<table id="middleTable">';
+					echo '<tr>';
+						if($i<count($hitboxOnline)+1){
+							//show hitboxOnline stream info
+							displayHitboxStreamInfo($hitboxOnline[$i-1]);
+						}else{
+							//show twitchOnline stream info
+							displayTwitchStreamInfo($twitchOnline[$i-1-count($hitboxOnline)]);
+						}
+					echo '</tr>';
+				echo '</table>';
+			echo '</td>';
 		}
 	}
 	function displayHitboxStreamInfo($stream){
 		echo '<td>';
-			echo '<a href="http://www.hitbox.tv/'.$stream.'" target="_blank"><img src="http://edge.sf.hitbox.tv'.getHitboxImage($stream).'" title="http://www.hitbox.tv/'.$stream.'" alt="'.$stream.'" style="height:35px; width:35px"></img></a>';
+			echo '<div id="box"><a href="http://www.hitbox.tv/'.$stream.'" target="_blank"><img src="http://edge.sf.hitbox.tv'.getHitboxImage($stream).'" title="http://www.hitbox.tv/'.$stream.'" alt="'.$stream.'" style="height:35px; width:35px"></img><div>'.getHitboxDescription($stream).'</div></a></div>';
 		echo '</td>';
 		echo '<td>';
 			echo '<table id="innerTable">';
