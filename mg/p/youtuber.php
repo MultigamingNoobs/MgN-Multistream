7b1fc2ec5a8a3f7582372b00cebb39b25894935c
MarderLP <Marder001@gmail.com>
2014-12-17 11:24:07 +0100

    code cleanup
    removed unused loc

--- mg/p/youtuber.php
+++ mg/p/youtuber.php
@@ -1,7 +1,6 @@
 <?php
 	$youtuber = array("marder007006005","teiwazlp","damakash","nephtisgaming","mgNB3rZ3rK3r","tomme90201","mgNKater","bayerwaldlp","brainiyak");
 	shuffle($youtuber);
-	//sort($youtuber);
 	for($i=0;$i<count($youtuber);$i++){
 		echo '<iframe frameborder="0" id="youtuber" scrolling="no" src="http://www.youtube.com/subscribe_widget?p='.$youtuber[$i].'"></iframe>';
 	}
