7266cfe0a0b913495dee8084b7efc1d84f6935de
MarderLP <Marder001@gmail.com>
2014-12-17 11:25:05 +0100

    moved code
    moved menu and share buttons into own files

---
+++ mg/shareButtons.php
@@ -0,0 +1,5 @@
+<div id="shareButtons">
+	<?php echo'<a href="https://twitter.com/intent/tweet?button_hashtag=MgNoobs&text="" class="twitter-hashtag-button" data-related="mgnmultistream" data-url="'.short('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']).'">Tweet #MgNoobs</a>';?>
+	<?php echo'<div class="g-plusone" data-size="medium" data-href="'.short("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']).'"></div>';?>
+	<?php echo'<div class="fb-like" data-href="'.short("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']).'" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>';?>
+</div>
