9dca32a954df7c378907a85d019ed5a6685c13a2
MarderLP <Marder001@gmail.com>
2014-12-17 11:52:00 +0100

    fixed index conflict from merge

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="de">
 	<head>
-<<<<<<< HEAD
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<!-- Android 5.0 Chrome-->
 		<meta name="theme-color" content="#330000">
@@ -38,46 +37,6 @@
 			include 'mg/api/streamApi.php';
 		?>
 		<title>MgN-Multistream</title>
-=======
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<!--css -->
-	<link href="mg/css/style.min.css" type="text/css" rel="stylesheet">
-	<link href="mg/api/pace/pace.css" rel="stylesheet" />
-	<link href="mg/api/mmenu/menu.css" rel="stylesheet" />
-	<link href="mg/api/chartist/chartist.min.css" rel="stylesheet" />
-	<!-- js -->
-	<script src="mg/api/jquery.js"></script>
-	<script src="mg/api/chartist/chartist.min.js"></script>
-	<script src="mg/api/mmenu/menu.js"></script>
-	<!-- Android 5.0 Chrome-->
-	<meta name="theme-color" content="#330000">
-	<link rel="icon" sizes="192×192" href="favicon.ico">
-	<!-- important php variables-->
-	<?php
-		//version of MgN-Multistream
-		$v = "v.0.9.1";
-		//language settings
-		$lang = 'en';
-		if($_GET['lang'] != null and $_GET['lang'] != ''){
-			$lang = strtolower($_GET['lang']);
-		}
-		include 'mg/api/lang/'.$lang.'.php';
-		//page
-		$p = strtolower($_GET['p']);
-		if($p == '' or $p == null){
-			$p=strtolower($home);
-		}
-		//google analyticstracking
-		include_once('mg/analyticstracking.php');
-		//google url shortner api (goo.gl)
-		include 'mg/api/short.php';
-		include 'mg/api/teamVars.php';
-		include 'mg/api/hitboxApi.php';
-		include 'mg/api/twitchApi.php';
-		include 'mg/api/streamApi.php';
-	?>
-	<title>MgN-Multistream</title>
->>>>>>> origin/v.0.9.1
 	</head>
 	<body>
 		<?php include 'mg/menu.php';?>
